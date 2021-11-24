8b94e0c10da1b8c87b0f3ab70f3b2bbe71c4a9fb
JosiahGuy <77808550+JosiahGuy@users.noreply.github.com>
2021-11-24 13:39:23 -0500

    Add files via upload

---
+++ yahoodata.py
@@ -0,0 +1,9 @@
+import pandas_datareader.data as pdr #Added to bring in Yahoo Finance Data
+import datetime as dt #Added to bring in Yahoo Finance Data
+
+def yahooData(ticker):
+    start = dt.datetime(2018, 1, 1)
+    end = dt.datetime(2021, 11, 17)
+    dataset = pdr.get_data_yahoo(ticker, start=start, end=end, interval='d')
+    dataset_filtered = dataset[["High", "Low", "Open", "Close"]]
+    return dataset_filtered
